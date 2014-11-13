3eb0281a0e905ebd6b3fcce4f31e052454f90038
Ben Russell <b4russell@gmail.com>
2014-11-12 17:09:46 -0800

    resolved index.html and timer.js conflicts

--- static/javascripts/timer.js
+++ static/javascripts/timer.js
@@ -1,21 +1,12 @@
 /*jslint browser: true, devel: true*/
 /*global $, jQuery, alert, FastClick, Handlebars*/
-<<<<<<< HEAD
-/*TODO
- * Fix delete last player
- * Fix sort
- */
-$(document).ready(function () {
-    "use strict";
-    var GAME_LENGTH = 10,
-=======
+
 $(document).ready(function () {
 
     "use strict";
 
     var GAME_LENGTH = 10,
 	INITIAL_GAME_LENGTH = 10,
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         MS_INCREMENT = 1000,
         SUM_TIMES = 0,
         AVERAGE_TIME = 0,
@@ -24,10 +15,7 @@ $(document).ready(function () {
         MIN_TIME = 0,
         MAX_TIME = 0,
         EDIT_MODE = false,
-<<<<<<< HEAD
-=======
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         totalTime = function (element) {
             var offset = 0;
             if (element.hasClass("running")) {
@@ -35,10 +23,6 @@ $(document).ready(function () {
             }
             return parseInt(element.attr("base"), 10) + offset;
         },
-<<<<<<< HEAD
-=======
-
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         refreshPlayers = function () {
             var numPlayers = $(".player").length;
             AVERAGE_TIME = SUM_TIMES / numPlayers + MS_INCREMENT;
@@ -48,23 +32,14 @@ $(document).ready(function () {
             SUM_TIMES = 0;
             SUM_DEVIATIONS = 0;
             $(".player").increment();
-<<<<<<< HEAD
-            $(".player").subout();
-=======
             $(".player").subsuggest();
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
             if (SUM_TIMES && !EDIT_MODE) {
                 $("#reset").removeClass("disabled");
             } else {
                 $("#reset").addClass("disabled");
             }
-<<<<<<< HEAD
-            console.log("standard deviation:" + STANDARD_DEVIATION + ", average: " + AVERAGE_TIME);
-        },
-=======
         },
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         reloadPlayers = function () {
             var i = 0,
                 storedPlayers = [],
@@ -87,22 +62,12 @@ $(document).ready(function () {
             }).loadPlayers();
             refreshPlayers();
         },
-<<<<<<< HEAD
-=======
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         resetTime = function (idx, elt) {
             var name = $(elt).find(".name").text().trim();
             localStorage[$(elt).attr('id')] = JSON.stringify({"name": name, "base": 0, "runstart": 0, "running": false});
             console.log($(elt).attr('id'));
         },
-<<<<<<< HEAD
-        resetTimes = function () {
-            $(".player").each(resetTime);
-            reloadPlayers();
-        },
-        playersTemplate = Handlebars.compile($('#player-template').html()),
-=======
 
         resetTimes = function () {
             $(".player").each(resetTime);
@@ -112,7 +77,6 @@ $(document).ready(function () {
 
         playersTemplate = Handlebars.templates.playerlist,
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         subToggle = function () {
             if ($(this).hasClass("running")) {
                 $(this).attr("base", totalTime($(this)));
@@ -123,10 +87,7 @@ $(document).ready(function () {
             $(this).store();
             $("#instructions").fadeOut();
         },
-<<<<<<< HEAD
-=======
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         deletePlayer = function (e) {
             var id = $(this).parent().attr('id');
             e.stopPropagation();
@@ -136,19 +97,13 @@ $(document).ready(function () {
                 $(".edit").show();
             });
         },
-<<<<<<< HEAD
-=======
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         appendPlayer = function (player, playerData) {
             $(".players").append(playersTemplate(
                 {"id": player, "name": playerData.name, "base": playerData.base, "runstart": playerData.runstart, "running": playerData.running ? " running" : ""}
             ));
         },
-<<<<<<< HEAD
-=======
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         addPlayer = function (player) {
             var playerName = encodeURIComponent(player.trim());
             player = player.trim();
@@ -159,14 +114,6 @@ $(document).ready(function () {
                 $("#newplayer").val('');
                 $('#confirmaddplayer').addClass("disabled");
             } else {
-<<<<<<< HEAD
-                console.log($('#' + playerName));
-            }
-        },
-        dt = new Date();
-//    var follow = function (bill, currentValue, callback) {
-    FastClick.attach(document.body);
-=======
                 //Don't add null players or players who are already on the team. Maybe add a user message 
                 console.log($('Could not add ' + playerName));
             }
@@ -176,7 +123,6 @@ $(document).ready(function () {
     //Make taps work on mobile
     FastClick.attach(document.body);
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
     Number.prototype.displayTime = function () {
         var ms = this,
             hours = Math.floor(ms / 3600000),
@@ -203,12 +149,6 @@ $(document).ready(function () {
         return this.each(function () {
             if ($(this).text()) {
                 var currentTime = totalTime($(this));
-<<<<<<< HEAD
-                $(this).find(".clock").text(currentTime.displayTime());
-                while (currentTime > GAME_LENGTH * 60 * 1000) {
-                    GAME_LENGTH *= 1.5;
-                }
-=======
                 //increment player's time displayed on clock
                 $(this).find(".clock").text(currentTime.displayTime());
 		//GAME_LENGTH determines how many minutes the player width represents. If a player's time exceeds this, GAME_LENGTH needs to be increased.
@@ -216,31 +156,18 @@ $(document).ready(function () {
                     GAME_LENGTH *= 1.5;
                 }
                 //grow the player's timer bar
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
                 $(this).find(".timebar").width($(this).width() * currentTime / (GAME_LENGTH * 60 * 1000));
             }
         });
     };
 
-<<<<<<< HEAD
-    $.fn.subout = function () {
-=======
     $.fn.subsuggest = function () {
         //make suggestions about which players should be subbed out or in
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         return this.each(function () {
             var currentTime = totalTime($(this)),
                 deviation = Math.pow(currentTime - AVERAGE_TIME, 2);
             SUM_TIMES += currentTime;
             SUM_DEVIATIONS += deviation;
-<<<<<<< HEAD
-            //if (currentTime < TEMP_MIN_TIME) {
-            //    TEMP_MIN_TIME = currentTime;
-            //} else if (currentTime > MAX_TIME) {
-            //    MAX_TIME = currentTime;
-            //}
-=======
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
             if (currentTime > MAX_TIME && $(this).hasClass("running") && STANDARD_DEVIATION > 60000) {
                 $(this).addClass("subout");
             } else {
@@ -263,33 +190,19 @@ $(document).ready(function () {
     };
 
     reloadPlayers();
-<<<<<<< HEAD
-    $(".players").on("click", ".player", subToggle);
-    $(".players").on("click", ".delete", deletePlayer);
-
-    $("#reset").click(function () {
-        //localStorage.clear();
-=======
 
     $(".players").on("click", ".player", subToggle);
 
     $(".players").on("click", ".delete", deletePlayer);
 
     $("#reset").click(function () {
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         if (!$(this).hasClass("disabled")) {
             resetTimes();
         }
     });
 
     $("#createplayer").click(function () {
-<<<<<<< HEAD
-        //$("#newplayer").val('');
         $("#newplayer").show();
-        //$("#newplayer").attr("contenteditable", "true");
-=======
-        $("#newplayer").show();
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         $("#newplayer").focus();
         $("#confirmaddplayer").show();
         $("#confirmaddplayer").addClass("disabled");
@@ -299,21 +212,6 @@ $(document).ready(function () {
         var playerName = $(this).siblings("#newplayer").val().toLowerCase();
         if (playerName) {
             addPlayer(playerName);
-<<<<<<< HEAD
-            $("#createplayer").show();
-            $(this).hide();
-            $('#newplayer').hide();
-//$("#newplayer").attr("contenteditable", "true");
-            //$("body, html").animate({ scrollTop: $(this).offset().top }, "slow");
-        }
-    });
-    $("#newplayer").focusin(function () {
-        $("#createplayer").hide();
-        $(this).outerWidth($(this).parent().width() - ($('#confirmaddplayer').outerWidth() + 1));
-        console.log(window.getSelection());
-    });
-    $("#newplayer").focusout(function () {
-=======
             //after adding a new player, the 'Add New Player' button reappears and input field and confirm button go away
             $("#createplayer").show();
             $(this).hide();
@@ -330,47 +228,18 @@ $(document).ready(function () {
 
     $("#newplayer").focusout(function () {
         //if input field is left empty and user clicks on another element, hide the input field and show the 'Add New Player' button (should be DRY'ed up with #confirmaddplayer.click above)
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         if (!$(this).val().trim()) {
             $("#createplayer").show();
             $(this).hide();
             $('#confirmaddplayer').hide();
         }
-<<<<<<< HEAD
-//$("#newplayer").attr("contenteditable", "false");
-        //if ($(this).val().trim()) {
-        //    $("#createplayer").removeClass('disabled');
-        //}
-        //$("#confirmaddplayer").hide();
-        //$(this).outerWidth($(this).parent().width() - ($('#confirmaddplayer').outerWidth() + $('#createplayer').outerWidth() + 1));
-    });
-=======
     });
 
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
     $("#editteam").click(function () {
         var currentText = $(this).text();
         EDIT_MODE = !$(".edit").is(":visible");
         $(".edit#newplayerrow").slideToggle(200);
         $(".edit.delete.button").toggle();
-<<<<<<< HEAD
-        //        left: parseInt($(this).css('left'), 10) === 0 ? -$(this).outerWidth() : 0
-        //});
-        $(this).text($(this).attr("toggletext"));
-        $(this).attr("toggletext", currentText);
-        //$("body, html").animate({ scrollTop: $(this).offset().top }, "slow");
-        if (EDIT_MODE) {
-            $("#reset").addClass("disabled");
-        }
-        //$(".timebar").toggle();
-    });
-
-    $("#newplayer").keyup(function (e) {
-        //var target = $(e.target);
-        //var focused = $(document.activeElement),
-        //    inputting = focused.get(0).tagName.toLowerCase() === "textarea" || focused.get(0).tagName.toLowerCase() === "input";
-        // / (forward slash) key = search
-=======
 	//Replace 'Edit' with 'Done Editing' (or whatever the alternate button label specified in toggletext is)
         $(this).text($(this).attr("toggletext"));
         $(this).attr("toggletext", currentText);
@@ -381,35 +250,21 @@ $(document).ready(function () {
 
     $("#newplayer").keyup(function (e) {
         //Only enable "Add" button after new player has a name
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
         if ($(this).val().trim()) {
             $("#confirmaddplayer").removeClass("disabled");
         } else {
             $("#confirmaddplayer").addClass("disabled");
         }
-<<<<<<< HEAD
-
-        if (e.which === 13) {
-            addPlayer($(this).val().toLowerCase());
-            e.preventDefault();
-            //$("#confirmaddplayer").click();
-=======
         
         //Add player on Enter key
         if (e.which === 13) {
             addPlayer($(this).val().toLowerCase());
             e.preventDefault();
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
             return;
         }
     });
 
-<<<<<<< HEAD
-
-
-=======
     //Refresh player times every MS_INCREMENT milliseconds
->>>>>>> bb404787ccce3f49e928d97edaf6bfaaecb2261b
     setInterval(refreshPlayers, MS_INCREMENT);
 
 });
